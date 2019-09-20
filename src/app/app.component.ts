d4212a843dfa905d672a51f87281e9978a7a2bdc
sarawatmanoj <jsonmanojkumar.com>
2019-09-20 16:50:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as jspdf from 'jspdf'; 
+ 
+import html2canvas from 'html2canvas'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-html-genrate-to-pdf';
+
+
+  public generatePDF() 
+ { 
+ var data = document.getElementById('contentToConvert'); 
+ html2canvas(data).then(canvas => { 
+ // Few necessary setting options 
+ var imgWidth = 208; 
+ var pageHeight = 295; 
+ var imgHeight = canvas.height * imgWidth / canvas.width; 
+ var heightLeft = imgHeight; 
+ 
+ const contentDataURL = canvas.toDataURL('image/png') 
+ let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF 
+ var position = 0; 
+ pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight) 
+ 
+ pdf.save('MYPdf.pdf'); // Generated PDF  
+ }); 
+ } 
 }
